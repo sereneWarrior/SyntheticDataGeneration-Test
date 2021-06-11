d396f00304a3bc824b29a8b1f5c9f30001edcf57
Anne-Marie Grabbe <56431940+AMGrabbe@users.noreply.github.com>
2021-06-11 15:57:07 +0200

    Add random object generation

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public ImageSynthesis synth;
+
+    public GameObject[] prefabs;
+    public int maxObjectsPerFrame;
+
+    private GameObject[] createdGameObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        createdGameObjects = new GameObject[maxObjectsPerFrame];
+        GenerateObjects();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        synth.OnSceneChange();
+        GenerateObjects();
+    }
+
+    private void GenerateObjects()
+    {
+        //TODO: Object pooling
+        foreach(var obj in createdGameObjects)
+        {
+            Destroy(obj);
+        }
+
+        //generate random 0..3
+        for (var i= 0; i < maxObjectsPerFrame; i++)
+        {
+            var randomPosition = new Vector3(
+                Random.Range(-11, 11),
+                Random.Range(5, 11),
+                Random.Range(11, -11)
+            ); 
+
+
+            var randomObject = Instantiate(prefabs[Random.Range(0,prefabs.Length)],
+            randomPosition, 
+            Random.rotation);
+
+            randomObject.GetComponent<Renderer>().material.color = Random.ColorHSV();
+            var scale = Random.Range(-3, 3);
+            randomObject.transform.localScale +=  new Vector3(
+                scale,
+                scale,
+                scale
+            );
+
+            createdGameObjects[i] = randomObject;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# SyntheticDataGeneration-Test
+
+## Synthetic Data Generation Test
+Test for generating synthetic training data for object detection with Unity.
+
+
+
+
+## Sources
+- This software uses Unity ML-ImageSynthsis - see ImageSynthesis-Library.txt.
+- Generating Synthetic Data for Image Segmentation with Unity and PyTorch/fastai - https://www.youtube.com/watch?v=P4CCMvtUohA
+- OBJECT POOLING in Unity - https://www.youtube.com/watch?v=tdSmKaJvCoA
